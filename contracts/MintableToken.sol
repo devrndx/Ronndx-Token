91c54a53e9dd0ab129dad2cbcbb35911b2f8f436
Dev <dev@round-x.com>
2022-06-17 18:16:23 +0900

    Revert "Remove to restart mint function"
    This reverts commit 45ecdca9de1d2025714978d4602c70d8238adda9.

--- contracts/MintableToken.sol
+++ contracts/MintableToken.sol
@@ -10,17 +10,19 @@ import "./OwnableToken.sol";
 // ----------------------------------------------------------------------------
 contract MintableToken is KIP7Mintable, OwnableToken{
     event MintFinished();
+    event MintReStart();
 
     bool public mintingFinished = false;
 
     modifier canMint() { require(!mintingFinished); _; }
+    modifier cantMint() { require(mintingFinished); _; }
 
     function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool){
         return super.mint(_to, _amount);
     }
 
     function addMinter(address account) onlyOwner public {
-        return
+        super.addMinter(account);
     }
 
     function renounceMinter() onlyOwner public {
@@ -35,6 +37,12 @@ contract MintableToken is KIP7Mintable, OwnableToken{
     function finishMinting() onlyOwner canMint public returns (bool) {
         mintingFinished = true;
         emit MintFinished();
+        return true;
+    }
+
+    function reStartMinting() onlyOwner cantMint public returns (bool) {
+        mintingFinished = false;
+        emit MintReStart();
         return true;
     }
 }
